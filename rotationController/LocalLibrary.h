5c6263d0ca7338b5724608add6e9efd63f992722
Jonas Scharpf <info@brainelectronics.de>
2016-08-18 16:45:05 +0200

    test commit after migrating to gogs

--- rotationController/LocalLibrary.h
+++ rotationController/LocalLibrary.h
@@ -18,21 +18,7 @@
 
 
 // Core library - IDE-based
-#if defined(WIRING) // Wiring specific
-#include "Wiring.h"
-#elif defined(MAPLE_IDE) // Maple specific
-#include "WProgram.h"
-#elif defined(MPIDE) // chipKIT specific
-#include "WProgram.h"
-#elif defined(DIGISPARK) // Digispark specific
-#include "Arduino.h"
-#elif defined(ENERGIA) // LaunchPad MSP430 G2 and F5529, Stellaris and Tiva, Experimeter Board FR5739 specific
-#include "Energia.h"
-#elif defined(MICRODUINO) // Microduino specific
-#include "Arduino.h"
-#elif defined(TEENSYDUINO) // Teensy specific
-#include "Arduino.h"
-#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#if defined(ARDUINO) // Arduino 1.0 and 1.5 specific
 #include "Arduino.h"
 #else // error
 #error Platform not defined
--- rotationController/rotationController.ino
+++ rotationController/rotationController.ino
@@ -152,11 +152,12 @@ void runMotor()
     Serial.println("clockwise");
     myStepper.step(stepsPerRevolution);
     delay(500);
-    
+    /*
     // step one revolution in the other direction:
     Serial.println("counterclockwise");
     myStepper.step(-stepsPerRevolution);
     delay(500);
+     */
 }
 
 //
